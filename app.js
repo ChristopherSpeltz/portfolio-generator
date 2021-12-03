e149b74cdf70d61c1b7fb5478b9ef2d17356dd51
Christopher Speltz <chrisspeltz@msn.com>
2021-12-02 23:05:08 -0600

    need to finish 9.5.5

--- app.js
+++ app.js
@@ -156,10 +156,19 @@ promptUser()
   .then((portfolioData) => {
     const pageHTML = generatePage(portfolioData);
     console.log(portfolioData)
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw Error(err);
-
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
         console.log('Page created! Check out index.html in this directory to see it!');
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('Style sheet copied successfully!');
+        });
     });
   });
 
